3e11f1c96da821bc07fbad1f597231e94c240a62
Okeke Chiemerie <charleschiemerie2004@gmail.com>
2026-05-01 16:32:43 +0100

    Fixed

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,3 +1,11 @@
 /// <reference types="vite/client" />
 
 declare module '*.css';
+// src/swiper-env.d.ts
+declare module 'swiper/css';
+declare module 'swiper/css/pagination';
+
+// Add any other Swiper CSS modules you might use in the future:
+declare module 'swiper/css/navigation';
+declare module 'swiper/css/scrollbar';
+declare module 'swiper/css/effect-fade';
